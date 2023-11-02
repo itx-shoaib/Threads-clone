0e8368e8e0c3aa12921c0ee59e820c797c9b33a5
itx-shoaib <shoaibjamil43@gmail.com>
2023-11-02 16:28:47 +0500

    Making onBoarding page

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,14 @@
+async function Page(){
+    return (
+        <main className="mx-auto max-w-3xl flex flex-col justify-start px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">Finish setting up your profile to access Threads.</p>
+
+            <section className="bg-dark-2 p-10 mt-9">
+                Action
+            </section>
+        </main>
+    )
+}
+
+export default Page;

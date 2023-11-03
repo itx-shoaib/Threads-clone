b7ad675a75567f1dff24194eefb94830c7a41487
itx-shoaib <shoaibjamil43@gmail.com>
2023-11-03 16:33:38 +0500

    Making the handleImage function

--- components/form/ActionProfile.tsx
+++ components/form/ActionProfile.tsx
@@ -18,7 +18,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 
 interface Props{
     user:{
@@ -33,6 +33,8 @@ interface Props{
 }
 
 const ActionProfile = ({user, btnTitle}: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues:{
@@ -49,8 +51,25 @@ const ActionProfile = ({user, btnTitle}: Props) => {
         console.log(values)
     }
 
-    const handleImage = (e: ChangeEvent, fieldChange : (value: string)=>void)=>{
+    const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange : (value: string)=>void)=>{
         e.preventDefault()
+
+        const fileReader = new FileReader();
+
+        if(e.target.files && e.target.files.length > 0 ){
+          const file = e.target.files[0];
+          setFiles(Array.from(e.target.files))
+          
+
+        if(!file.type.includes("image")) return;
+
+        fileReader.onload = async (event) => {
+          const imageDataUrl = event.target?.result?.toString() || "";
+          fieldChange(imageDataUrl);
+        };
+  
+        fileReader.readAsDataURL(file);
+      }
     }
 
   return (
